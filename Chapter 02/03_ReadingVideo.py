e044a11e941878f1d90c3df9a0ac503f1fe6e347
Misbah Sehar <misbahsahar09@gmail.com>
2025-12-01 22:36:55 +0500

    gray output video

--- Chapter 02/03_ReadingVideo.py
+++ Chapter 02/03_ReadingVideo.py
@@ -20,7 +20,9 @@ print("Resolution:", width, "x", height)
 
 # Create VideoWriter to save output
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+
 out = cv2.VideoWriter("Chapter 02/images/output.mp4", fourcc, fps, (width, height))
+out2 = cv2.VideoWriter("Chapter 02/images/output_gray.mp4", fourcc, fps, (width, height))
 
 # Read, Display, and Save frames
 while True:
@@ -34,12 +36,14 @@ while True:
 
     # 0 flip vertically, 1 flip horizontally, -1 flip both
     flipped_frame = cv2.flip(frame, -1)  # horizontal flip
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Show flipped video
     cv2.imshow("Video", flipped_frame)
 
     # out.write()
     out.write(flipped_frame)
+    out2.write(cv2.cvtColor(gray_frame, cv2.COLOR_GRAY2BGR))
 
     # Quit manually
     if cv2.waitKey(25) & 0xFF == ord('q'):
@@ -47,4 +51,5 @@ while True:
 
 cap.release()
 out.release()
+out2.release()
 cv2.destroyAllWindows()
