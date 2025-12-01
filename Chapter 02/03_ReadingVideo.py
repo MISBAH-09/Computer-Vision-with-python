79b49f9c43629620488d3801ae8a0af3712290b6
Misbah Sehar <misbahsahar09@gmail.com>
2025-12-01 22:34:39 +0500

    read write video

---
+++ Chapter 02/03_ReadingVideo.py
@@ -0,0 +1,50 @@
+import cv2
+
+# Path to video file
+video_path = "Chapter 02/images/input.mp4"
+
+cap = cv2.VideoCapture(video_path)
+
+# Check if video opened successfully
+if not cap.isOpened():
+    print("Error: Could not open video.")
+    exit()
+
+# Get video properties 
+fps = cap.get(cv2.CAP_PROP_FPS)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+print("FPS:", fps)
+print("Resolution:", width, "x", height)
+
+# Create VideoWriter to save output
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+out = cv2.VideoWriter("Chapter 02/images/output.mp4", fourcc, fps, (width, height))
+
+# Read, Display, and Save frames
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("End of video reached.")
+        break
+
+    # cv2.imshow("Video", frame)
+
+    # 0 flip vertically, 1 flip horizontally, -1 flip both
+    flipped_frame = cv2.flip(frame, -1)  # horizontal flip
+
+    # Show flipped video
+    cv2.imshow("Video", flipped_frame)
+
+    # out.write()
+    out.write(flipped_frame)
+
+    # Quit manually
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
